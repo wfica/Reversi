9a37745c12a7dc276ea694db6b3d63b7761fbeb9
wfica <wojtekfica@gmail.com>
2018-05-29 21:43:40 +0200

    random plays working - almost

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,9 @@
 module Main
 where
 
-import           Reversi
-import qualified Reversi.Board                 as B
 import qualified Reversi.State                 as S
-import           Reversi.Gameplay
+import           Reversi.RandomPlay
 
 main :: IO ()
-main = do 
-    print a
-    print b
-    mapM_ S.printNicely log 
-    print "-----------"
-    mapM_ S.printNicely (S.getIthGeneration stateTest 0)
-    print "-----------"
-    mapM_ S.printNicely (S.getIthGeneration stateTest 1)
-    where  
-        (a, b, log) = playOneGameWithLog
-
-
-
-boardTest = B.strToBoard "__________________________OOO______OOO______OOOX___OXXXX__OXXXXX"
-stateTest = State boardTest X Nothing (S.generateMoves stateTest)
+main = randomGame >>= S.printNicely
 
---
+++ src/Reversi/RandomPlay.hs
@@ -0,0 +1,29 @@
+--{-# LANGUAGE TypeSynonymInstances, FlexibleContexts, NoMonomorphismRestriction, FlexibleInstances #-}
+
+module Reversi.AgentRandom
+where
+
+import           Reversi
+import qualified Reversi.State                 as S
+
+import           System.Random
+
+
+
+makeRandomMove :: State -> IO (Maybe State)
+makeRandomMove state = if len == 0
+    then return Nothing
+    else Just . (!!) nexts <$> randomRIO (0, len - 1)
+  where
+    nexts = getNext state
+    len   = length nexts
+
+randomGame :: IO State
+randomGame = go S.initial
+  where
+    go :: State -> IO State
+    go s = do
+        maybeNext <- makeRandomMove s
+        case maybeNext of
+            Nothing   -> return s
+            Just next -> go next
