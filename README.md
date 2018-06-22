b762b01926f4b2a1f2614940fddacb58e038babb
wfica <wojtekfica@gmail.com>
2018-06-22 10:37:13 +0200

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# reversi
+# Reversi random generator
+
+Prosuces random rounds of reversi.
+
+Compilation: ```stack build```
+
+Run: ```stack exec reversi-exe```
+
+
--- src/Reversi/RandomPlay.hs
+++ src/Reversi/RandomPlay.hs
@@ -9,20 +9,25 @@ import qualified Reversi.State                 as S
 import           System.Random
 
 
+infixl 9 !#! -- takes n-th element of a list 
+(!#!) :: [a] -> Int -> Maybe a
+[] !#! _         = Nothing
+xs !#! n | n < 0 = Nothing
+(x : _ ) !#! 0   = Just x
+(_ : xs) !#! n   = xs !#! (n - 1)
+
 makeRandomMove :: State -> IO (Maybe State)
-makeRandomMove state = if len == 0
-    then return Nothing
-    else Just . (!!) nexts <$> randomRIO (0, len - 1)
-  where
-    nexts = getNext state
-    len   = length nexts
+makeRandomMove state = (!#!) nexts <$> randomRIO (0, len - 1)
+ where
+  nexts = getNext state
+  len   = length nexts
 
 randomGame :: IO State
 randomGame = go S.initial
-  where
-    go :: State -> IO State
-    go s = do
-        maybeNext <- makeRandomMove s
-        case maybeNext of
-            Nothing   -> return s
-            Just next -> go next
+ where
+  go :: State -> IO State
+  go s = do
+    maybeNext <- makeRandomMove s
+    case maybeNext of
+      Nothing   -> return s
+      Just next -> go next
